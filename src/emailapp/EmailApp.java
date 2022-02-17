59a6d11f7bc8b2e228125665e26677927264411e
chris0512 <75781998+chris0512@users.noreply.github.com>
2022-02-17 09:48:54 +0800

    Initial commit

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,47 @@
+package emailapp;
+import java.util.*;
+public class EmailApp {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        // User Info
+        System.out.println("Enter first name:");
+        String f_name = s.next();
+        System.out.println("Enter last name:");
+        String l_name = s.next();
+
+        // creating object for Email class
+        Email em1 = new Email(f_name, l_name);
+        int choice = -1;
+        do{
+            System.out.println("\n******\nEnter your choice \n1.Show Info\n2.Change Password\n3. Change mailbox capacity\n4.Set Alternate mail\n5.Store data in file\n6.Dispaly the data\n7.exit ");
+            choice = s.nextInt();
+            switch(choice){
+                case 1:
+                    em1.getInfo();
+                    break;
+                case 2:
+                    em1.set_password();
+                    break;
+                case 3:
+                    em1.set_mailCap();
+                    break;
+                case 4:
+                    em1.alternate_email();
+                    break;
+                case 5:
+                    em1.store_file();
+                    break;
+                case 6:
+                    em1.read_file();
+                    break;
+                case 7:
+                    System.out.println("Thank you for using this app");
+                    break;
+                default:
+                    System.out.println("Invalid choice enter\n proper choice again...");
+
+            }
+        }while(choice != 5);
+    }
+}
